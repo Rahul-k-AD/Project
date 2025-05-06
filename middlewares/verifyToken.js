811e44961d74ed81c34bd6b7753c6a14b4a97770
Rahul-K-AD <rahulk.ad24@bitsathy.ac.in>
2025-05-06 15:23:09 +0530

    Updates Made

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,6 +1,21 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization')?.replace('Bearer ', '');
+function verifyToken(req, res, next) {
+    const authHeader = req.header('Authorization')
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if (!token) return res.status(401).json({ message: 'No token provided' }) // checking if token is provided in the request header
+
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => { // verifying the token using jsonwebtoken library and the secret key from .env file
+        if (err) {
+            return res.status(403).json({ message: 'Invalid token' }) // checking if token is valid
+        } // checking if token is valid
+        // console.log(user) // logging the user data from the token
+        req.user = user // setting the user in the request object
+        next() // calling the next middleware or route handler
+    })
 }
+
+module.exports = verifyToken // exporting the verifyToken middleware to be used in other files
--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');  // importing libraries
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const verifyToken = require('./middlewares/verifyToken'); // importing the verifyToken middleware
 require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000; // crating express app and setting port
 const authRoutes = require('./routes/auth'); // importing the auth routes
+const adminRoutes = require('./routes/admin'); // importing the admin routes
 
 
 app.use(cors());  //allowing API access from different domains/ports (like frontend runs on localhost 3000) allowing it to call this api
 app.use(bodyParser.json()); // making server understand json data
 app.use('/api/auth', authRoutes); // using the auth routes in the express app
-
-
+app.use('/api/admin', adminRoutes); // using the admin routes in the express app
 app.get('/', (req, res) => {
     res.send('Engines are running!');
 }); // creating a test route to check if the server is running
